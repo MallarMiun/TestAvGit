641bd081d8e4cb005119ecede1bbe8e5d65f7a13
Malin Larsson <malin.larsson@miun.se>
2020-12-14 17:11:24 +0100

    InitialCreate

---
+++ TestAvGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestAvGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
